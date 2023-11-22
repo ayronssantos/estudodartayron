63926bf29476564df1cbf30796c260765963ef02
Ayron Santos <ayronssantos@gmail.com>
2023-11-22 14:27:57 -0300

    Inicio Estudo Dart

---
+++ bin/dartestudoayron.dart
@@ -0,0 +1,12 @@
+//introdução a variaveis
+void main() {
+  String nome = "Ayron Santos";
+  int valorvariavel = 10;
+  bool everdadeiro = true;
+  List<String> listadepalavras = ["Ayron", "Santos"];
+
+  print(nome);
+  print(valorvariavel);
+  print(everdadeiro);
+  print("${listadepalavras[0]} - ${listadepalavras[1]}");
+}
---
+++ test/dartestudoayron_test.dart
@@ -0,0 +1,8 @@
+import 'package:dartestudoayron/dartestudoayron.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
